45b830c4b055c3db7ecbe4a488ee0f5a5b3e053f
flavien-hugs <geekactux@gmail.com>
2022-07-09 19:11:51 +0000

    edit config for deployement

--- config.py
+++ config.py
@@ -2,8 +2,12 @@
 
 import os
 
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'tasks.db')
+if os.environ.get('DATABASE_URL') is None:
+   BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+   SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'tasks.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
