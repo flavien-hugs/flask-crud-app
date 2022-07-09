6831e4f8bc027c167682c84d9d5c28e879ccc723
flavien-hugs <geekactux@gmail.com>
2022-07-09 21:05:13 +0000

    improve config

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ if os.environ.get('DATABASE_URL') is None:
    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'tasks.db')
 else:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+   SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
