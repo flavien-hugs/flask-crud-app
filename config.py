e379615f7c136be0cf1c390ef89e48b429c546d7
flavien-hugs <geekactux@gmail.com>
2022-07-09 15:22:29 +0000

    edit project environment key configuration

---
+++ config.py
@@ -0,0 +1,12 @@
+# task.config.py
+
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+if os.environ.get('DATABASE_URL') is None:
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'tasks.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
