52cf0f3fce51f77f4d3b6eb1e029f2a35ba8c10d
flavien-hugs <geekactux@gmail.com>
2022-07-09 22:03:34 +0000

    #FixBug database

--- config.py
+++ config.py
@@ -1,16 +1,12 @@
 # task.config.py
 
 import os
-import re
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-if os.environ.get('DATABASE_URL') is None:
+if os.getenv('DATABASE_URL'):
+   SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+else:
    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'tasks.db')
-else:
-   uri = os.getenv("DATABASE_URL")
-   if uri.startswith("postgres://"):
-      uri = uri.replace("postgres://", "postgresql://", 1)
-      SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
