a5490c456ba8fe77295641e698c20259eba4769d
flavien-hugs <geekactux@gmail.com>
2022-07-09 15:21:30 +0000

    improve task views

--- task/views.py
+++ task/views.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+# task.views.py
+
+from flask import Flask, redirect, url_for, render_template
+
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 
 @app.route("/")
-@app.route("/index/")
 def index():
     return render_template("pages/index.html")
 
@@ -12,3 +15,13 @@ def index():
 @app.route("/about/")
 def about():
     return render_template("pages/about.html")
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('pages/page_not_found.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('pages/page_not_found.html'), 500
