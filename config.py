6bbc0df3a8e2c2241cc4d8e1323c97d56829201c
flavien-hugs <geekactux@gmail.com>
2022-07-09 21:53:17 +0000

    improve production config

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # task.config.py
 
 import os
+import re
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -9,4 +10,7 @@ if os.environ.get('DATABASE_URL') is None:
    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'tasks.db')
 else:
-   SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+   uri = os.getenv("DATABASE_URL")
+   if uri.startswith("postgres://"):
+      uri = uri.replace("postgres://", "postgresql://", 1)
+      # SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
